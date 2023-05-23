daa65eb7e322d44379386b23c1ced659bf6784fb
marynghiem <marynghiem46@gmail.com>
2023-05-23 13:17:27 -0700

    added information for quality control page

--- src/components/QualityControl.js
+++ src/components/QualityControl.js
@@ -1,4 +1,30 @@
 import "../css/QualityControl.css";
 import React, { useState } from "react";
+import { Navbar } from "./Navbar";
 
-export const QualityControl = () => {};
+export const QualityControl = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2>Quality Control</h2>
+      <div>
+        We are committed to meeting the expectations of our customers all of the time by teamwork, innovation and
+        competence, exhititing high ethical standards, a sense of urgency and respect for the individual. We expect no
+        less from our suppliers.
+      </div>
+      <h2>INSPECTION EQUIPMENT</h2>
+      <div>
+        COORDINATE MEASURING MACHINE, Mitutoyo, CNC Model BRT 504m 20" x 16" x 16", travel, utilizing "Geo Pak:"
+        software fully programmable. COORDINATE MEASURING MACHINE, Mitutoyo, Model B231, 28" x 24" x 18", travel,
+        utilizing "Geo Pak 100PC" software. OPTICAL COMPARATOR, Micro-UV, Model H14 with surface and profile
+        illumination, utilizing a Q16 Metrology computer. SURFACE ROUGHNESS TESTER, Mitutoyo, Surftest 211 Fisher
+        coating thickness gage. 12" Bench Center, 36" x 48" "Crystal Pink" Starett Surface Plate. 24" x 18" Granite
+        Surface Plates, 12" x 18" Granite Surface Plate. Precision Gage Block sets (working and calibrated). 0" - 12"
+        Outside Micrometer Sets. 0" – 1" & 0" – 2" Indicating Micrometers, .00005 Resolution. 0" – 1" & 0" – 2" Screw
+        Thread Micrometers. .011" - .750" Diameter Precision Gage Pin sets. Assorted Precision Deltronic Pins in
+        increments of .0001" (as required). Digital Calipers thru 24". Precision 5" Sine Bar. .200" - 2" Inside
+        Micrometers. .700" - 2.500" Dial Bore Gages in increments of .0001". Thread Plug Gages (as required).
+      </div>
+    </div>
+  );
+};
