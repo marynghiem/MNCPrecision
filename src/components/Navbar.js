712aa97a4e418e7e6d6716f01e57d644a75d1d4b
marynghiem <marynghiem46@gmail.com>
2023-05-30 13:17:50 -0700

    fixed navbar for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,23 +4,35 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="navMenuList">
-      <img src="mncprecisionlogo.png" />
-      <Link to="/" className="navMenuHome">
-        Home
-      </Link>
-      <Link to="/Services" className="navMenuServices">
-        Services
-      </Link>
-      <Link to="/Equipment" className="navMenuEquipment">
-        Equipment
-      </Link>
-      <Link to="/QualityControl" className="navMenuQualityControl">
-        Quality Control
-      </Link>
-      <Link to="/ContactUs" className="navMenuContactUs">
-        Contact Us
-      </Link>
+    <div className="header">
+      <div className="navMenu">
+        <img src="mncprecisionlogo.png" />
+        <div className="navMenuListItem">
+          <a href="/" className="navMenuHome">
+            Home
+          </a>
+        </div>
+        <div className="navMenuListItem">
+          <a href="/Services" className="navMenuServices">
+            Services
+          </a>
+        </div>
+        <div className="navMenuListItem">
+          <a href="/Equipment" className="navMenuEquipment">
+            Equipment
+          </a>
+        </div>
+        <div className="navMenuListItem">
+          <a href="/QualityControl" className="navMenuQualityControl">
+            Quality Control
+          </a>
+        </div>
+        <div className="navMenuListItem">
+          <a href="/ContactUs" className="navMenuContactUs">
+            Contact Us
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
