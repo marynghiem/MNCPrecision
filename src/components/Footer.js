0d73921bf8dbf2c018c3a192200dd327bfc657f0
marynghiem <marynghiem46@gmail.com>
2023-10-12 12:44:01 -0700

    adjusted the margin spacing so the footer content was not so close to each other

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,23 @@ import "../css/Footer.css";
 import React, { useState } from "react";
 
 export const Footer = () => {
-  return <div className="footerContainer"></div>;
+  return (
+    <div className="footerContainer">
+      <div className="footerCompanyName footersections">MNC Precision</div>
+      <div className="footerAddress footersections">
+        <div className="footerAddressTitle ">ADDRESS</div>
+        <div className="footerAddressDescription"> 1020 Commercial St., Suite #104A San Jose Ca 95112</div>
+      </div>
+      <div className="footerPhoneNumber footersections">
+        <div className="footerPhoneNumberTitle">PHONE NUMBER</div>
+        <div className="footerPhoneNumberDescription">
+          Office : (408) 437-9034 Fax: (408) 437-3934 Cell: (408) 966-5282
+        </div>
+      </div>
+      <div className="footerEmail footersections">
+        <div className="footerEmailTitle">EMAIL</div>
+        <div className="footerEmailDescription">info@mncprecision.com</div>
+      </div>
+    </div>
+  );
 };
