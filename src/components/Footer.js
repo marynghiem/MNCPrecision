f55316a9c1e9f8f84ed37de4b40cc675284fc704
marynghiem <marynghiem46@gmail.com>
2023-10-18 12:52:08 -0700

    fixed the styling of footer by making the font white and spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,21 +9,27 @@ export const Footer = () => {
     <div className="footerContainer">
       <div className="footerCompanyName footersections">MNC Precision</div>
       <div className="footerAddress footersections">
-        <LocationOnIcon />
-        <div className="footerAddressTitle ">ADDRESS</div>
-        <div className="footerAddressDescription"> 1020 Commercial St., Suite #104A San Jose Ca 95112</div>
+        <div className="iconBorderBottom">
+          <LocationOnIcon sx={{ fontSize: 80 }} />
+        </div>
+        <div className="footerAddressTitle title">ADDRESS</div>
+        <div className="footerAddressDescription descriptions"> 1020 Commercial St., Suite #104A San Jose Ca 95112</div>
       </div>
       <div className="footerPhoneNumber footersections">
-        <PhoneIphoneIcon />
-        <div className="footerPhoneNumberTitle">PHONE NUMBER</div>
-        <div className="footerPhoneNumberDescription">
+        <div className="iconBorderBottom">
+          <PhoneIphoneIcon sx={{ fontSize: 80 }} />
+        </div>
+        <div className="footerPhoneNumberTitle title">PHONE NUMBER</div>
+        <div className="footerPhoneNumberDescription descriptions">
           Office : (408) 437-9034 Fax: (408) 437-3934 Cell: (408) 966-5282
         </div>
       </div>
       <div className="footerEmail footersections">
-        <EmailIcon />
-        <div className="footerEmailTitle">EMAIL</div>
-        <div className="footerEmailDescription">info@mncprecision.com</div>
+        <div className="iconBorderBottom">
+          <EmailIcon sx={{ fontSize: 80 }} />
+        </div>
+        <div className="footerEmailTitle title">EMAIL</div>
+        <div className="footerEmailDescription descriptions">info@mncprecision.com</div>
       </div>
     </div>
   );
